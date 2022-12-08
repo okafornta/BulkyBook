adc2c0d02f0a31687b8b4805e0867bc3e8d15827
Okafornta <hillaryonia@gmail.com>
2022-12-08 14:18:10 +0100

    Add project files.

---
+++ BulkyBookweb/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using BulkyBookweb.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookweb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var objCategoryList = _db.categories.ToList();
+            return View(objCategoryList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+
+}
---
+++ BulkyBookweb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BulkyBookweb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookweb.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> categories { get; set; }
+        public IEnumerable<Category> Categories { get; internal set; }
+    }
+}
